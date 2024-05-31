fc006565e8a9517f1d077ac6ea2e60d18f74f943
Chelsiiy <151575298+Chelsiiy@users.noreply.github.com>
2024-05-31 09:04:39 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello, Dev!</Text>
+    <br> <Text style={styles.text}>Hello, Dev!</Text> </br>
+      <text style={styles.Smalltext}> 14 tasks today</text> ,
       <StatusBar style="auto" />
     </View>
   );
@@ -19,5 +20,9 @@ const styles = StyleSheet.create({
   },
   text: {
     textAlign: 'left',
+    fontSize:40 ,
   },
 });
+
+
+
